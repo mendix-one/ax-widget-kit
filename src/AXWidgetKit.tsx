90a2c4dd40417e71f85ea2884bd326ec42b58c6b
Danniel Nguyen <dannielnguyen289@gmail.com>
2026-03-30 21:17:02 +0900

    Init project

---
+++ src/AXWidgetKit.tsx
@@ -0,0 +1,10 @@
+import { ReactElement } from "react";
+import { HelloWorldSample } from "./components/HelloWorldSample";
+
+import { AXWidgetKitContainerProps } from "../typings/AXWidgetKitProps";
+
+import "./ui/AXWidgetKit.css";
+
+export function AXWidgetKit({ sampleText }: AXWidgetKitContainerProps): ReactElement {
+    return <HelloWorldSample sampleText={sampleText ? sampleText : "World"} />;
+}
